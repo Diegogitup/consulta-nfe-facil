61b51231f066259c1d18974ca1cfd34cd151924a
Diegogitup <diego.dsilva51@gmail.com>
2026-06-16 07:46:46 -0300

    add axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,22 @@
 export async function buscarXml(chave) {
-  const result = await fetch(
-    "http://localhost:3000/api/nfe",
+  console.log("Chamando backend...");
+
+  const response = await fetch(
+    "https://consulta-nfe-facil-back.onrender.com/api/nfe",
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        chave: chave,
-      }),
+      body: JSON.stringify({ chave }),
     }
   );
 
-  const dados = await result.json();
+  console.log("Status:", response.status);
+
+  const dados = await response.json();
+
+  console.log("Dados recebidos:", dados);
 
   return dados;
 }
